2b1ffbfa45ac20510c4a5554f2dd059a521da0bd
Philip Samaras <Philipsamaras@gmail.com>
2024-08-20 11:59:27 +0000

    Change the URL and Views paths

--- resturant/urls.py
+++ resturant/urls.py
@@ -1,8 +1,9 @@
+from django.urls import path, include
 from . import views
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('', views.index, name="index"),
 
 ]
--- resturant/views.py
+++ resturant/views.py
@@ -1,42 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views import generic
-from .models import Post
-from .models import News
+from .models import News, Post
 
+def index(request):
+    return render(request, 'resturant/index.html')
 
-
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
-    template_name = "resturant/index.html"
-    paginate_by = 6
-
-
-
-def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
-    """
-
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
-
-
-
-def home(request):
-    news_list = News.objects.all()
-    return render(request, 'index.html', {'news_list': news_list})
